75bd746a2bbc2cb1d6b0d5fd99f56e691bea6d37
DrProtato <danielgreeff@outlook.com>
2020-05-20 11:44:14 +1000

    Update dnd5e stat roll.py
    cleaned up while loop to take up less lines

--- dnd5e stat roll.py
+++ dnd5e stat roll.py
@@ -3,15 +3,11 @@ j = 0
 i = 0
 statlist = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"]
 while i < 6:
-	dice = []
-	a = random.randint(1,6)
-	dice.append(a)
-	b = random.randint(1,6)
-	dice.append(b)
-	c = random.randint(1,6)
-	dice.append(c)
-	d = random.randint(1,6)
-	dice.append(d)
+	dice = [a,b,c,d]
+	dice[a] = random.randint(1,6)
+	dice[b] = random.randint(1,6)
+	dice[c] = random.randint(1,6)
+	dice[d] = random.randint(1,6)
 	dice.sort()
 	stat = dice[1] + dice[2] + dice[3]
 	print(statlist[i] + ": " + str(stat))
