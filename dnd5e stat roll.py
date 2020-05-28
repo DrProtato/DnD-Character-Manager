ffb8de7c85cb8ef2632786ab2253f0e46c7a41ae
DrProtato <danielgreeff@outlook.com>
2020-05-28 10:23:38 +1000

    Update dnd5e stat roll.py

--- dnd5e stat roll.py
+++ dnd5e stat roll.py
@@ -4,10 +4,10 @@ i = 0
 statlist = ["Strength", "Dexterity", "Constitution", "Intelligence", "Wisdom", "Charisma"]
 while i < 6:
 	dice = [a,b,c,d]
-	dice[a] = random.randint(1,6)
-	dice[b] = random.randint(1,6)
-	dice[c] = random.randint(1,6)
-	dice[d] = random.randint(1,6)
+	dice[0] = random.randint(1,6)
+	dice[1] = random.randint(1,6)
+	dice[2] = random.randint(1,6)
+	dice[3] = random.randint(1,6)
 	dice.sort()
 	stat = dice[1] + dice[2] + dice[3]
 	print(statlist[i] + ": " + str(stat))
